2df9217000801707dba7096319c39164a1d7317d
NeatDeveloper <ShahTheShah@ya.ru>
2023-12-16 12:27:27 +0300

    Init project

--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    let number = $state(0);
-</script>
-
-<h1>Number is { number }!</h1>
-<button on:click={() => number++}>Increment</button>
-<button on:click={() => number--}>Decrement</button>
