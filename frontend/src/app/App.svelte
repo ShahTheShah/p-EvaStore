6ad515b77c155c090cdc6d1316492c78bae1073b
NeatDeveloper <ShahTheShah@ya.ru>
2023-12-17 20:15:11 +0300

    Init project

---
+++ frontend/src/app/App.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import { writable } from "svelte/store";
+
+</script>
+
+<h1>Hello, world!</h1>
---
+++ frontend/src/index.ts
@@ -0,0 +1,8 @@
+import '$app/index.scss';
+import App from '$app/App.svelte';
+
+const app = new App({
+    target: document.getElementById('app') as HTMLElement,
+});
+
+export default app;
